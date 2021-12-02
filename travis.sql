5af8b5de33cc3655b0179e79cd115ee9dbec3667
Sigroy <70609482+Sigroy@users.noreply.github.com>
2021-12-02 09:31:55 -0600

    Update travis.sql

--- travis.sql
+++ travis.sql
@@ -4,9 +4,9 @@ GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
 CREATE DATABASE IF NOT EXISTS `pruebas_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `pruebas_db`;
 
-CREATE TABLE IF NOT EXISTS `alumnos_tb1` (
-  `id` int(20),
-  `nombre` varchar(80),
-  `email` varchar(90),
-  `edad` int(20)
+CREATE TABLE IF NOT EXISTS `alumnos_tbl` (
+  `id` int(11),
+  `nombre` varchar(50),
+  `email` varchar(50),
+  `edad` int(11)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
